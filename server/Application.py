01048f71d86a1d2ed43bef5aa23321e429d4887d
Celo <94490465+3h-coder@users.noreply.github.com>
2024-06-16 19:29:04 +0200

    Update Application.py

--- server/Application.py
+++ server/Application.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from blueprints.play import play_bp
 from config.config import global_config
 from config.logger import logger
 from config.variables import RequiredVariables
@@ -19,6 +20,7 @@ class Application(Flask):
     def initialize(self):
         self.set_config()
         self.register_middlewares()
+        self.register_blueprints()
 
     def set_config(self):
         self.config["SECRET_KEY"] = global_config[RequiredVariables.APP_SECRET_KEY.name]
@@ -28,3 +30,9 @@ class Application(Flask):
         Registers all the middlewares present in the middlewares package.
         """
         self.register_error_handler(Exception, handle_error)
+
+    def register_blueprints(self):
+        """
+        Registers all the blueprints present in the blueprints package
+        """
+        self.register_blueprint(play_bp)
