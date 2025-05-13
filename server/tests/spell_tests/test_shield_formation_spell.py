43a4b32b4cdd3d7a1e53b2de855ac51f2adc54f5
3h-coder <94490465+3h-coder@users.noreply.github.com>
2025-05-13 23:38:37 +0200

    preparing shield formation spell tests

---
+++ server/tests/spell_tests/test_shield_formation_spell.py
@@ -0,0 +1,13 @@
+from tests.helpers.match_helper import MatchHelper
+
+
+def test_shield_formation_should_not_be_usable(started_match: MatchHelper):
+    pass
+
+
+def test_shield_formation_works(started_match: MatchHelper):
+    pass
+
+
+def test_shielded_effect(started_match: MatchHelper):
+    pass
